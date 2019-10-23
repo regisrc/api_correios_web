be0099d9b4516c8c940d943197f9a5fcf5d4c291
darkeght <darkeght@gmail.com>
2019-10-23 08:17:33 -0300

    Envaindo

--- SolucaoApiCorreios/AplicacaoWebCorreios/Utils/scripts.js
+++ SolucaoApiCorreios/AplicacaoWebCorreios/Utils/scripts.js
@@ -1,1 +1,7 @@
-﻿
+﻿$(document).ready(function () {
+    $('#buttonEnviar').click(function () {
+        $.get("http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?nCdEmpresa=08082650&sDsSenha=564321&sCepOrigem=70002900&sCepDestino=04547000&nVlPeso=1&nCdFormato=1&nVlComprimento=20&nVlAltura=20&nVlLargura=20&sCdMaoPropria=n&nVlValorDeclarado=0&sCdAvisoRecebimento=n&nCdServico=04510&nVlDiametro=0&StrRetorno=xml&nIndicaCalculo=3", function (data) {
+            console.log(data)
+        });
+    });
+});
