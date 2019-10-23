9b480afbdfb42738ccc72542cae767328f40bde4
Douglas Cuco <douglas.cuco@gmail.com>
2019-10-23 11:28:52 -0300

    script pronto para fazer a front dos retornos

--- SolucaoApiCorreios/AplicacaoWebCorreios/Utils/scripts.js
+++ SolucaoApiCorreios/AplicacaoWebCorreios/Utils/scripts.js
@@ -12,13 +12,51 @@
             nVlComprimento : $('#nVlComprimento').val(),
             nVlAltura : $('#nVlAltura').val(),
             nVlLargura : $('#nVlLargura').val(),
-            nVlDiametro : $('#nVlDiametro').val(),
+            nVlDiametro : 0,
             sCdMaoPropria : "n",
             nVlValorDeclarado : 0,
             sCdAvisoRecebimento : "n"
         }
         $.post("http://usysweb.com.br/api/correiosambev.php?giomar=true", obj, function (data) {
             alert("Data: " + data);
+            var objJson = JSON.parse(data);
+
+            if (objJson.cServico.Erro != "0") {
+                alert("Erro " + objJson.cServico.Erro + "\r\nMensagem " + objJson.cServico.MsgErro);
+                return;
+            }
+
+            $('#codigo').val(objJson.cServico.Codigo)
+
+            switch (objJson.cServico.EntregaDomiciliar) {
+
+                case "S":
+                    $('#entregaDomi').val("Tem entrega domiciliar");
+                    break;
+                case "N":
+                    $('#entregaDomi').val("Nao tem entrega domiciliar");
+                    break;
+            }
+            switch (objJson.cServico.EntregaSabado) {
+
+                case "S":
+                    $('#entregaSab').val("Entrega sabado!");
+                    break;
+                case "N":
+                    $('#entregaSab').val("Nao entrega sabado");
+                    break;
+            }
+
+           // $('#entregaDomi').val(objJson.cServico.EntregaDomiciliar);
+           // $('#entregaSab').val(objJson.cServico.EntregaSabado);
+           // $('#idErro').val(objJson.cServico.Erro);
+           // $('#msgErro').val(objJson.cServico.MsgErro);
+            $('#prazoEntrega').val(objJson.cServico.PrazoEntrega);
+            $('#valor').val(objJson.cServico.Valor);
+            $('#valorReceb').val(objJson.cServico.ValorAvisoRecebimento);
+            $('#valorMao').val(objJson.cServico.ValorMaoPropria);
+            $('#valorSemAdd').val(objJson.cServico.ValorSemAdicionais);
+            $('#valorDec').val(objJson.cServico.ValorValorDeclarado);
         });
     });
 });
