f04c19b75e583433c7d87b420d012697d2d2542d
Douglas Cuco <douglas.cuco@gmail.com>
2019-10-23 09:35:59 -0300

    Script output pronto

--- SolucaoApiCorreios/AplicacaoWebCorreios/Utils/scripts.js
+++ SolucaoApiCorreios/AplicacaoWebCorreios/Utils/scripts.js
@@ -1,7 +1,12 @@
 ﻿$(document).ready(function () {
     $('#buttonEnviar').click(function () {
-        $.get("http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?nCdEmpresa=08082650&sDsSenha=564321&sCepOrigem=70002900&sCepDestino=04547000&nVlPeso=1&nCdFormato=1&nVlComprimento=20&nVlAltura=20&nVlLargura=20&sCdMaoPropria=n&nVlValorDeclarado=0&sCdAvisoRecebimento=n&nCdServico=04510&nVlDiametro=0&StrRetorno=xml&nIndicaCalculo=3", function (data) {
-            console.log(data)
+
+        var url = "http://usysweb.com.br/api/correiosambev.php?" + `nCdEmpresa=&sDsSenha=&nCdServico=${$('#nCdServico').val()}&sCepOrigem=${$('#sCepOrigem').val()}&sCepDestino=${$('#sCepDestino').val()}&nVlPeso=${$('#nVlPeso').val()}&nCdFormato=1&nVlComprimento=${$('#nVlComprimento').val()}&nVlAltura=${$('#nVlAltura').val()}&nVlLargura=${$('#nVlLargura').val()}&nVlDiametro=${$('#nVlDiametro').val()}&sCdMaoPropria=s&nVlValorDeclarado=20&sCdAvisoRecebimento=n`;
+        $.get((url), function (data) {
+            alert("Data: " + data);
         });
     });
 });
+
+
+
