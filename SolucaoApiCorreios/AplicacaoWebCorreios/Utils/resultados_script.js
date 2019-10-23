97e8697055cda0d540c7c27d6c6d90cbbab8ee37
Douglas Cuco <douglas.cuco@gmail.com>
2019-10-23 17:18:43 -0300

    tudo 100% só falta arrumar o html :p

---
+++ SolucaoApiCorreios/AplicacaoWebCorreios/Utils/resultados_script.js
@@ -0,0 +1,48 @@
+﻿$(document).ready(function () {
+
+
+    switch (queryString("entregaDomicilio")) {
+
+    case "S":
+        $('#entregaDomicilio').val("Tem entrega a domicilio");
+        break;
+    case "N":
+        $('#entregaDomicilio').val("Nao tem entrega a domicilio");
+        break;
+    }
+    switch (queryString("entregaSab")) {
+
+    case "S":
+        $('#entregaSab').val("Entrega sabado!");
+        break;
+    case "N":
+        $('#entregaSab').val("Nao entrega sabado");
+        break;
+    }
+
+    $('#prazoEntrega').val(queryString("prazoEntrega"));
+    $('#valor').val(queryString("valor"));
+    $('#valorReceb').val(queryString("valorReceb"));
+    $('#valorMao').val(queryString("valorMao"));
+    $('#valorSemAdd').val(queryString("valorSemAdd"));
+    $('#valorDec').val(queryString("valorDec"));
+
+});
+
+function queryString(parameter) {
+    var loc = location.search.substring(1, location.search.length);
+    var param_value = false;
+    var params = loc.split("&");
+    for (i = 0; i < params.length; i++) {
+        param_name = params[i].substring(0, params[i].indexOf('='));
+        if (param_name == parameter) {
+            param_value = params[i].substring(params[i].indexOf('=') + 1)
+        }
+    }
+    if (param_value) {
+        return param_value;
+    }
+    else {
+        return undefined;
+    }
+}
--- SolucaoApiCorreios/AplicacaoWebCorreios/Utils/scripts.js
+++ SolucaoApiCorreios/AplicacaoWebCorreios/Utils/scripts.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function () {
     $('#buttonEnviar').click(function () {
-
+        
         var obj = {
             nCdEmpresa : "",
             sDsSenha : "",
@@ -18,48 +18,21 @@
             sCdAvisoRecebimento : "n"
         }
         $.post("http://usysweb.com.br/api/correiosambev.php?giomar=true", obj, function (data) {
-            alert("Data: " + data);
+            //alert("Data: " + data);
             var objJson = JSON.parse(data);
 
             if (objJson.cServico.Erro != "0") {
                 alert("Erro " + objJson.cServico.Erro + "\r\nMensagem " + objJson.cServico.MsgErro);
                 return;
             }
-
-            $('#codigo').val(objJson.cServico.Codigo)
-
-            switch (objJson.cServico.EntregaDomiciliar) {
-
-                case "S":
-                    $('#entregaDomi').val("Tem entrega domiciliar");
-                    break;
-                case "N":
-                    $('#entregaDomi').val("Nao tem entrega domiciliar");
-                    break;
-            }
-            switch (objJson.cServico.EntregaSabado) {
-
-                case "S":
-                    $('#entregaSab').val("Entrega sabado!");
-                    break;
-                case "N":
-                    $('#entregaSab').val("Nao entrega sabado");
-                    break;
-            }
-
-           // $('#entregaDomi').val(objJson.cServico.EntregaDomiciliar);
-           // $('#entregaSab').val(objJson.cServico.EntregaSabado);
-           // $('#idErro').val(objJson.cServico.Erro);
-           // $('#msgErro').val(objJson.cServico.MsgErro);
-            $('#prazoEntrega').val(objJson.cServico.PrazoEntrega);
-            $('#valor').val(objJson.cServico.Valor);
-            $('#valorReceb').val(objJson.cServico.ValorAvisoRecebimento);
-            $('#valorMao').val(objJson.cServico.ValorMaoPropria);
-            $('#valorSemAdd').val(objJson.cServico.ValorSemAdicionais);
-            $('#valorDec').val(objJson.cServico.ValorValorDeclarado);
+            passaValor("entregaSab=" + objJson.cServico.EntregaSabado, "prazoEntrega=" + objJson.cServico.PrazoEntrega, "entregaDomicilio=" + objJson.cServico.EntregaDomiciliar, "valor=" + objJson.cServico.Valor, "valorReceb=" + objJson.cServico.ValorAvisoRecebimento, "valorMao=" + objJson.cServico.ValorMaoPropria, "valorSemAdd=" + objJson.cServico.ValorSemAdicionais, "valorDec="+objJson.cServico.ValorValorDeclarado);
         });
     });
 });
 
 
+var passaValor = function (var1,var2,var3,var4,var5,var6,var7,var8) {
+    var url = "resultados.html?" + var1 + "&" + var2 + "&" + var3 + "&" + var4 + "&" + var5 + "&" + var6 + "&" + var7 + "&" + var8;
+    window.location = url;
+}
 
