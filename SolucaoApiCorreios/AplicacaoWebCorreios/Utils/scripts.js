948e7f397de5ad0d63162445bcbddb99767d3ef3
Douglas Cuco <douglas.cuco@gmail.com>
2019-10-23 10:26:43 -0300

    funcionando com post

--- SolucaoApiCorreios/AplicacaoWebCorreios/Utils/scripts.js
+++ SolucaoApiCorreios/AplicacaoWebCorreios/Utils/scripts.js
@@ -1,8 +1,23 @@
 ﻿$(document).ready(function () {
     $('#buttonEnviar').click(function () {
 
-        var url = "http://usysweb.com.br/api/correiosambev.php?" + `nCdEmpresa=&sDsSenha=&nCdServico=${$('#nCdServico').val()}&sCepOrigem=${$('#sCepOrigem').val()}&sCepDestino=${$('#sCepDestino').val()}&nVlPeso=${$('#nVlPeso').val()}&nCdFormato=1&nVlComprimento=${$('#nVlComprimento').val()}&nVlAltura=${$('#nVlAltura').val()}&nVlLargura=${$('#nVlLargura').val()}&nVlDiametro=${$('#nVlDiametro').val()}&sCdMaoPropria=s&nVlValorDeclarado=20&sCdAvisoRecebimento=n`;
-        $.get((url), function (data) {
+        var obj = {
+            nCdEmpresa : "",
+            sDsSenha : "",
+            nCdServico : $('#nCdServico').val(),
+            sCepOrigem : $('#sCepOrigem').val(),
+            sCepDestino : $('#sCepDestino').val(),
+            nVlPeso : $('#nVlPeso').val(),
+            nCdFormato : 1,
+            nVlComprimento : $('#nVlComprimento').val(),
+            nVlAltura : $('#nVlAltura').val(),
+            nVlLargura : $('#nVlLargura').val(),
+            nVlDiametro : $('#nVlDiametro').val(),
+            sCdMaoPropria : "n",
+            nVlValorDeclarado : 0,
+            sCdAvisoRecebimento : "n"
+        }
+        $.post("http://usysweb.com.br/api/correiosambev.php?giomar=true", obj, function (data) {
             alert("Data: " + data);
         });
     });
