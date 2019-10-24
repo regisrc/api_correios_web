39ff026dac1059d6bff811bbe91b9c8093c50294
Douglas Cuco <douglas.cuco@gmail.com>
2019-10-24 15:24:55 -0300

    Mais bonitin do que nunca
    falta validação dos campos antes de buscas, e utulizar o serialized

--- SolucaoApiCorreios/AplicacaoWebCorreios/Utils/resultados_script.js
+++ SolucaoApiCorreios/AplicacaoWebCorreios/Utils/resultados_script.js
@@ -1,25 +1,50 @@
 ﻿$(document).ready(function () {
 
+    switch (queryString("codServico")) {
+
+        case "04014":
+            $('#codServico').val("04014 SEDEX à vista");
+            break;
+        case "04065":
+            $('#codServico').val("04065 SEDEX à vista pagamento na entrega");
+            break;
+        case "04510":
+            $('#codServico').val("04510 PAC à vista");
+            break;
+        case "04707":
+            $('#codServico').val("04707 PAC à vista pagamento na entrega");
+            break;
+        case "40169":
+            $('#codServico').val("40169 SEDEX 12 (à vista e a faturar)*");
+            break;
+        case "40215":
+            $('#codServico').val("40215 SEDEX 10 (à vista e a faturar)*");
+            break;
+        case "40290":
+            $('#codServico').val("40290 SEDEX Hoje Varejo*");
+            break;
+
+
+    }
 
     switch (queryString("entregaDomicilio")) {
 
     case "S":
-        $('#entregaDomicilio').val("Tem entrega a domicilio");
+        $('#entregaDomicilio').val("Tem entrega a domicílio");
         break;
     case "N":
-        $('#entregaDomicilio').val("Nao tem entrega a domicilio");
+        $('#entregaDomicilio').val("Não tem entrega a domicílio");
         break;
     }
     switch (queryString("entregaSab")) {
 
     case "S":
-        $('#entregaSab').val("Entrega sabado!");
+        $('#entregaSab').val("Entrega sábado!");
         break;
     case "N":
-        $('#entregaSab').val("Nao entrega sabado");
+        $('#entregaSab').val("Não entrega sábado");
         break;
     }
-
     $('#prazoEntrega').val(queryString("prazoEntrega"));
     $('#valor').val(queryString("valor"));
     $('#valorReceb').val(queryString("valorReceb"));
--- SolucaoApiCorreios/AplicacaoWebCorreios/Utils/scripts.js
+++ SolucaoApiCorreios/AplicacaoWebCorreios/Utils/scripts.js
@@ -1,21 +1,50 @@
 ﻿$(document).ready(function () {
+
+    $('#sCepOrigem').mask("99999-999");
+    $('#sCepDestino').mask("99999-999");
+    $('#nCdFormato').change(function () {
+        switch ($('#nCdFormato').val()) {
+            case "1":
+                $('#nVlAltura').removeAttr('disabled');
+                $('#nVlLargura').removeAttr('disabled');
+                $('#nVlDiametro').attr('disabled', 'disabled');
+                $('#nVlDiametro').val("0");
+                break;
+
+            case "2":
+                $('#nVlDiametro').removeAttr('disabled');
+                $('#nVlAltura').attr('disabled', 'disabled');
+                $('#nVlAltura').val("0");
+                $('#nVlLargura').attr('disabled', 'disabled');
+                $('#nVlLargura').val("0");
+                break;
+
+            case "3":
+                $('#nVlAltura').attr('disabled', 'disabled');
+                $('#nVlAltura').val("0");
+                $('#nVlDiametro').attr('disabled', 'disabled');
+                $('#nVlDiametro').val("0");
+                $('#nVlLargura').removeAttr('disabled');
+                break;
+        }
+    });
     $('#buttonEnviar').click(function () {
-        
+
         var obj = {
             nCdEmpresa : "",
             sDsSenha : "",
-            nCdServico : $('#nCdServico').val(),
-            sCepOrigem : $('#sCepOrigem').val(),
-            sCepDestino : $('#sCepDestino').val(),
+            nCdServico: $('#nCdServico').val(),
+            sCepOrigem: $('#sCepOrigem').val().replace("-",""),
+            sCepDestino: $('#sCepDestino').val().replace("-", ""),
             nVlPeso : $('#nVlPeso').val(),
-            nCdFormato : 1,
+            nCdFormato: $('#nCdFormato').val(),
             nVlComprimento : $('#nVlComprimento').val(),
             nVlAltura : $('#nVlAltura').val(),
             nVlLargura : $('#nVlLargura').val(),
-            nVlDiametro : 0,
-            sCdMaoPropria : "n",
-            nVlValorDeclarado : 0,
-            sCdAvisoRecebimento : "n"
+            nVlDiametro: $('#nVlDiametro').val(),
+            sCdMaoPropria: $('input[name="maoPropria"]:checked').val(),
+            nVlValorDeclarado: $('#nVlValorDeclarado').val(),
+            sCdAvisoRecebimento: $('input[name="avisoReceb"]:checked').val()
         }
         $.post("http://usysweb.com.br/api/correiosambev.php?giomar=true", obj, function (data) {
             //alert("Data: " + data);
@@ -25,14 +54,14 @@
                 alert("Erro " + objJson.cServico.Erro + "\r\nMensagem " + objJson.cServico.MsgErro);
                 return;
             }
-            passaValor("entregaSab=" + objJson.cServico.EntregaSabado, "prazoEntrega=" + objJson.cServico.PrazoEntrega, "entregaDomicilio=" + objJson.cServico.EntregaDomiciliar, "valor=" + objJson.cServico.Valor, "valorReceb=" + objJson.cServico.ValorAvisoRecebimento, "valorMao=" + objJson.cServico.ValorMaoPropria, "valorSemAdd=" + objJson.cServico.ValorSemAdicionais, "valorDec="+objJson.cServico.ValorValorDeclarado);
+            passaValor("entregaSab=" + objJson.cServico.EntregaSabado, "prazoEntrega=" + objJson.cServico.PrazoEntrega, "entregaDomicilio=" + objJson.cServico.EntregaDomiciliar, "valor=" + objJson.cServico.Valor, "valorReceb=" + objJson.cServico.ValorAvisoRecebimento, "valorMao=" + objJson.cServico.ValorMaoPropria, "valorSemAdd=" + objJson.cServico.ValorSemAdicionais, "valorDec=" + objJson.cServico.ValorValorDeclarado, "codServico=" + objJson.cServico.Codigo);
         });
     });
 });
 
 
-var passaValor = function (var1,var2,var3,var4,var5,var6,var7,var8) {
-    var url = "resultados.html?" + var1 + "&" + var2 + "&" + var3 + "&" + var4 + "&" + var5 + "&" + var6 + "&" + var7 + "&" + var8;
+var passaValor = function (var1,var2,var3,var4,var5,var6,var7,var8,var9) {
+    var url = "resultados.html?" + var1 + "&" + var2 + "&" + var3 + "&" + var4 + "&" + var5 + "&" + var6 + "&" + var7 + "&" + var8 + "&" + var9;
     window.location = url;
 }
 
