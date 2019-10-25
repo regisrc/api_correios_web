f116c940e11d79989d86d7e1b1169fd8f2d17c73
alinede0liveira <alinedeoliveira344@gmail.com>
2019-10-25 10:35:19 -0300

    Implementando url dinamica

--- SolucaoApiCorreios/AplicacaoWebCorreios/Utils/scripts.js
+++ SolucaoApiCorreios/AplicacaoWebCorreios/Utils/scripts.js
@@ -30,38 +30,43 @@
     });
     $('#buttonEnviar').click(function () {
 
-        var obj = {
-            nCdEmpresa : "",
-            sDsSenha : "",
-            nCdServico: $('#nCdServico').val(),
-            sCepOrigem: $('#sCepOrigem').val().replace("-",""),
-            sCepDestino: $('#sCepDestino').val().replace("-", ""),
-            nVlPeso : $('#nVlPeso').val(),
-            nCdFormato: $('#nCdFormato').val(),
-            nVlComprimento : $('#nVlComprimento').val(),
-            nVlAltura : $('#nVlAltura').val(),
-            nVlLargura : $('#nVlLargura').val(),
-            nVlDiametro: $('#nVlDiametro').val(),
-            sCdMaoPropria: $('input[name="maoPropria"]:checked').val(),
-            nVlValorDeclarado: $('#nVlValorDeclarado').val(),
-            sCdAvisoRecebimento: $('input[name="avisoReceb"]:checked').val()
-        }
+        var obj = $("#form_principal").serializeArray();
+        console.log(obj)
+        for (let index = 0; index < obj.length; index++)
+            if (obj[index].name == "sCepOrigem" || obj[index].name == "sCepDestino")
+                obj[index].value = obj[index].value.replace("-", "")
+
+        obj.push({
+            name: "nCdEmpresa",
+            value: ""
+        }, {
+            name: "sDsSenha",
+            value: ""
+        })
+
         $.post("http://usysweb.com.br/api/correiosambev.php?giomar=true", obj, function (data) {
             //alert("Data: " + data);
             var objJson = JSON.parse(data);
-
+            
             if (objJson.cServico.Erro != "0") {
                 alert("Erro " + objJson.cServico.Erro + "\r\nMensagem " + objJson.cServico.MsgErro);
                 return;
             }
-            passaValor("entregaSab=" + objJson.cServico.EntregaSabado, "prazoEntrega=" + objJson.cServico.PrazoEntrega, "entregaDomicilio=" + objJson.cServico.EntregaDomiciliar, "valor=" + objJson.cServico.Valor, "valorReceb=" + objJson.cServico.ValorAvisoRecebimento, "valorMao=" + objJson.cServico.ValorMaoPropria, "valorSemAdd=" + objJson.cServico.ValorSemAdicionais, "valorDec=" + objJson.cServico.ValorValorDeclarado, "codServico=" + objJson.cServico.Codigo);
+            passaValorTelaNew(objJson);
+            //passaValor("entregaSab=" + objJson.cServico.EntregaSabado, "prazoEntrega=" + objJson.cServico.PrazoEntrega, "entregaDomicilio=" + objJson.cServico.EntregaDomiciliar, "valor=" + objJson.cServico.Valor, "valorReceb=" + objJson.cServico.ValorAvisoRecebimento, "valorMao=" + objJson.cServico.ValorMaoPropria, "valorSemAdd=" + objJson.cServico.ValorSemAdicionais, "valorDec=" + objJson.cServico.ValorValorDeclarado, "codServico=" + objJson.cServico.Codigo);
         });
     });
 });
 
+var passaValorTelaNew = (url) => {
+    var urlElements = `resultados.html?`;
+    for (let index = 0; index < url.cServico.length; index++) {
+        urlElements += `${url.cServico[index].name}=${url.cServico[index].value}&`
+        
+    }
+}
 
 var passaValor = function (var1,var2,var3,var4,var5,var6,var7,var8,var9) {
     var url = "resultados.html?" + var1 + "&" + var2 + "&" + var3 + "&" + var4 + "&" + var5 + "&" + var6 + "&" + var7 + "&" + var8 + "&" + var9;
-    window.location = url;
+    //window.location = url;
 }
-
